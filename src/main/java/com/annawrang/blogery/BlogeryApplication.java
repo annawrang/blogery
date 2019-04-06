8ed0fb80a0083f57ff2df40d22ed52cb679f3bc5
Anna Wrang <anna.wrang@gmail.com>
2019-04-06 15:51:49 +0100

    Base project

---
+++ src/main/java/com/annawrang/blogery/BlogeryApplication.java
@@ -0,0 +1,13 @@
+package com.annawrang.blogery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogeryApplication.class, args);
+	}
+
+}
