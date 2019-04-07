97d1cf63a0ece0ada00c2f83b3f5519a12af9ddc
Anna Wrang <anna.wrang@gmail.com>
2019-04-07 13:55:53 +0100

    Create account

---
+++ src/main/java/com/annawrang/blogery/service/AccountService.java
@@ -0,0 +1,56 @@
+package com.annawrang.blogery.service;
+
+import com.annawrang.blogery.exception.BadRequestException;
+import com.annawrang.blogery.model.Account;
+import com.annawrang.blogery.repository.AccountRepository;
+import com.annawrang.blogery.resource.AccountResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import java.util.Set;
+import java.util.UUID;
+
+@Component
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private Validator validator;
+
+    /**
+     * Sign up - needs email and password provided
+     **/
+    public AccountResource createAccount(final AccountResource resource) {
+        validateResource(resource, "email");
+        validateResource(resource, "password");
+
+        accountRepository.findByEmail(resource.getEmail())
+                .ifPresent(p -> {
+                    throw new BadRequestException("Email address is already registered");
+                });
+
+        Account account = new Account()
+                .setAccountId(UUID.randomUUID())
+                .setEmail(resource.getEmail())
+                .setPassword(resource.getPassword());
+        return convert(accountRepository.save(account));
+    }
+
+    private void validateResource(final AccountResource resource, final String property) {
+        final Set<ConstraintViolation<AccountResource>> violations = validator.validateProperty(resource, property);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+
+    private AccountResource convert(Account account) {
+        return new AccountResource()
+                .setAccountId(account.getAccountId())
+                .setEmail(account.getEmail());
+    }
+}
---
+++ src/test/groovy/com/annawrang/blogery/service/account/CreateAccountTest.groovy
@@ -0,0 +1,51 @@
+package com.annawrang.blogery.service.account
+
+
+import com.annawrang.blogery.repository.AccountRepository
+import com.annawrang.blogery.resource.AccountResource
+import com.annawrang.blogery.service.AccountService
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.validation.ConstraintViolationException
+import javax.validation.Validation
+
+class CreateAccountTest extends Specification {
+
+    AccountService target
+
+    AccountRepository accountRepository
+
+    def setup() {
+        accountRepository = Mock(AccountRepository)
+        target = new AccountService(
+                accountRepository: accountRepository,
+                validator: Validation.buildDefaultValidatorFactory().getValidator()
+        )
+    }
+
+    @Unroll
+    def 'createAccount should throw an exception if the #scenario'() {
+        given:
+        def resource = accountResource(email, password)
+        when:
+        target.createAccount(resource)
+        then:
+        thrown(ConstraintViolationException)
+        where:
+        email            | password     | scenario
+        null             | 'Secret123'  | 'email is null'
+        ''               | 'Secret123'  | 'email is empty'
+        'invalid.email'  | 'Secret123'  | 'email is invalid format'
+        'anna@gmail.com' | null         | 'password is null'
+        'anna@gmail.com' | 'secret1234' | 'password is missing upper case'
+        'anna@gmail.com' | 'Secret1'    | 'password is too short'
+    }
+
+    def accountResource(email = 'anna@gmail.com', password = 'Secret123') {
+        new AccountResource(
+                email: email,
+                password: password
+        )
+    }
+}
